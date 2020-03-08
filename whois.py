8f1ff56d4e5a8c8d397beac0d3ae1ebc7a49c73c
EHoll <ehollidge@hotmail.com>
2020-03-08 13:04:34 +0100

    Update whois.py
    Added Better Regex for Url
    Added Report Button

--- whois.py
+++ whois.py
@@ -4,10 +4,11 @@ from tkinter.font import Font
 import requests as rq
 import re
 import json as js
+import webbrowser as wb
 
 url = "http://dotnul.com/api/whois/"
 window = tk.Tk()
-urlreg = '(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+urlreg = '(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)'
 bgcolour = "#222629"
 btncolour = "#86c232"
 btnpcolour = "#61892F"
@@ -16,16 +17,37 @@ txtcolour = "#ffffff"
 txtbcolour = "#6b6e70"
 lsthcolour = "#474A4F"
 
+def reportpopup(og_url):
+	print("hey")
+
+def create_mailto(email, domain, webaddress):
+	url = "mailto:" + email
+	url += "?subject=Possible abuse at " + domain + "&"
+	url +="body=Hello,\n\nI have recently come across what seems to be a spam/abusive" + \
+		" URL which seems to have been\npurchased  from your company. The domain name is " + domain + \
+		" . The web address I came across which appeared to be spam/abusive was " + webaddress + \
+		" .\n\nKind Regards\n"
+	url = url.replace(' ', '%20')
+	url = url.replace('\n', '%0D%0A')
+	return url
+
+def reportabuse(lst):
+	ary = lst.get(0, 'end')
+	if (len(ary) == 0):
+		return
+	wb.open(create_mailto("e", "e", "e"), new=1)
+
 def popup(msg):
 	popup = tk.Tk()
 	popup.title("Popup")
 	popup.iconbitmap("logo.ico")
-	w = 250
+	popup.configure(bg=bgcolour)
+	w = 350
 	h = 150
 	sw = popup.winfo_screenwidth()
 	sh = popup.winfo_screenheight()
 	popup.geometry('%dx%d+%d+%d' % (w, h, (sw / 2) - (w / 2), (sh / 2) - (h / 2)))
-	lbl = tk.Label(popup, text=msg, anchor="center")
+	lbl = tk.Label(popup, text=msg, anchor="center", bg=bgcolour, font=fnt, fg=txtcolour)
 	lbl.pack()
 	popup.mainloop()
 
@@ -42,7 +64,7 @@ def lookup(frm, lst):
 	response = rq.get(url + val)
 	json = js.loads(response.content)
 	if "No match for " in json["whois"]:
-		popup("The Response was invalid")
+		popup("The response or domain was invalid")
 		return
 	info = json["whois"].split("<br />")
 	lst.delete(0, lst.size())
@@ -58,17 +80,28 @@ def setup_gui(win):
 	win.configure(background=bgcolour)
 	sw = win.winfo_screenwidth()
 	sh = win.winfo_screenheight()
-	win.geometry('920x500+%d+%d' % ((sw / 2) - 250, (sh / 2) - 150))
+	win.geometry('920x500+%d+%d' % ((sw / 2) - 460, (sh / 2) - 250))
 	frm = tk.Frame(win, height=150, width=500, bg=bgcolour)
-	lbl = tk.Label(frm, text="Domain To Lookup : ", bg=bgcolour, font=fnt, fg=txtcolour)
+	lbl = tk.Label(frm, text="Domain To Lookup : ", bg=bgcolour,
+		font=fnt, fg=txtcolour)
 	lbl.grid(row=0, column=0, pady=10)
-	inp = tk.Entry(frm, name="domainin", width=50, bg=txtbcolour, font=fnt, fg=txtcolour, bd=0)
+	inp = tk.Entry(frm, name="domainin", width=50, bg=txtbcolour,
+		font=fnt, fg=txtcolour, bd=0)
 	inp.grid(row=0, column=1, pady=10)
-	lst = tk.Listbox(frm, name="lstbox", height=25, width=85, bg=txtbcolour, font=fnt, fg=txtcolour, selectbackground=lsthcolour)
+	lst = tk.Listbox(frm, name="lstbox", height=25, width=85, bg=txtbcolour,
+		font=fnt, fg=txtcolour, selectbackground=lsthcolour)
 	lst.grid(row=1, column=1)
 	lst.configure(bd=0, highlightthickness=0)
-	go = tk.Button(frm, text="Look Up", command= lambda: lookup(frm, lst), name="search", bg=btncolour, font=fnt, fg=txtcolour, activebackground=btnpcolour, activeforeground=txtcolour)
+	go = tk.Button(frm, text="Look Up", command= lambda: lookup(frm, lst),
+		name="search", bg=btncolour,
+		font=fnt, fg=txtcolour, activebackground=btnpcolour,
+		activeforeground=txtcolour)
 	go.grid(row=0, column=2, pady=10)
+	report = tk.Button(frm, text="Report", command=lambda: reportabuse(lst),
+		name="report", width=20, bg=btncolour, font=fnt,
+		fg=txtcolour, activebackground=btnpcolour,
+		activeforeground=txtcolour)
+	report.grid(row=2, column=1, pady=10)
 	frm.pack()
 
 def main():
